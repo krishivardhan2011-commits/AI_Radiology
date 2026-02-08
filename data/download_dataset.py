5964d9755129f26ee73d9337cea5d7661f336191
krishivardhan2011-commits <krishivardhan2011@gmail.com>
2026-02-08 16:23:39 +0530

    Create download_dataset.py

---
+++ data/download_dataset.py
@@ -0,0 +1,13 @@
+"""
+Dataset Instructions
+
+Primary Dataset:
+- MIMIC-CXR
+
+Benchmark Dataset:
+- IU Chest X-Ray
+
+Preprocessing performed in Google Colab environment.
+"""
+
+print("Refer README for dataset setup instructions.")
